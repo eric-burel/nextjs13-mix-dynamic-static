051c96f773e0205c8627eed1ddd52fe5e5d7fde4
eric-burel <eb@lebrun-burel.com>
2023-01-20 15:20:49 +0100

    demo issue

--- src/app/[lang]/[userId]/page.tsx
+++ src/app/[lang]/[userId]/page.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
 
+// will trigger ISR = 1 page is stored per user
+export const generateStaticParams = () => [];
+
+// will produce Error: A required parameter (userId) was not provided as a string in generateStaticParams for /[lang]/[userId]
+// export const dynamic = "force-dynamic";
+
 export default function UserProfile({ params }: any) {
+  console.log("Rerendering user profile page");
   return (
     <div>
       <p>Params from UserProfile: {JSON.stringify(params)}</p>
